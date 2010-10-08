8bc6ca08d276f3ce804093ce090c013b407fd2bd
daihuaye <daihua.ye@gmail.com>
2010-10-07 23:10:00 -0700

    update blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,7 @@ class ArticlesController < ApplicationController
   # GET /articles/1.xml
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new(:article=>@article)
 
     respond_to do |format|
       format.html # show.html.erb
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,25 @@
 class CommentsController < ApplicationController
   # GET /comments
   # GET /comments.xml
-  def index
-    @comments = Comment.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @comments }
-    end
-  end
+  # def index
+  #   @comments = Comment.all
+  # 
+  #   respond_to do |format|
+  #     format.html # index.html.erb
+  #     format.xml  { render :xml => @comments }
+  #   end
+  # end
 
   # GET /comments/1
   # GET /comments/1.xml
-  def show
-    @comment = Comment.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @comment }
-    end
-  end
+  # def show
+  #   @comment = Comment.find(params[:id])
+  # 
+  #   respond_to do |format|
+  #     format.html # show.html.erb
+  #     format.xml  { render :xml => @comment }
+  #   end
+  # end
 
   # GET /comments/new
   # GET /comments/new.xml
@@ -41,15 +41,11 @@ class CommentsController < ApplicationController
   # POST /comments.xml
   def create
     @comment = Comment.new(params[:comment])
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to(@comment, :notice => 'Comment was successfully created.') }
-        format.xml  { render :xml => @comment, :status => :created, :location => @comment }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
-      end
+    if @comment.save
+      flash[:notice] = "Successfully created comment."
+      redirect_to article_url(@comment.article_id)
+    else
+      render :action => 'new'
     end
   end
 
